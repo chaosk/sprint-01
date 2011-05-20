4553affa849a6806b17a4fbc089bcbd1a10ff519
Krzysztof Socha <ksocha@ksocha.com>
2011-05-21 00:24:10 +0200

    added edit task view

--- sprint-01/tasks/views.py
+++ sprint-01/tasks/views.py
@@ -1,9 +1,9 @@
 from django.core.urlresolvers import reverse
-from django.shortcuts import redirect
+from django.shortcuts import get_object_or_404, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
-from tasks.forms import NewTaskForm
+from tasks.forms import NewTaskForm, EditTaskForm, EditSharedTaskForm
 from tasks.models import Task
 from annoying.decorators import render_to
 
@@ -33,4 +33,31 @@ def task_add(request):
 
 	return {
 		'form': form,
+	}
+
+
+@render_to()
+@login_required
+def task_edit(request, task_id):
+	task = get_object_or_404(Task, pk=task_id)
+
+	if request.user in task.collaborators.all():
+		form_name = EditSharedTaskForm
+		template = 'tasks/edit_shared.html'
+	elif task.created_by == request.user:
+		form_name = EditTaskForm
+		template = 'tasks/edit.html'
+
+	form = form_name(instance=task)
+
+	if request.method == 'POST':
+		form = form_name(request.POST, instance=task)
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Task \"{0}\" updated.".format(task))
+			return redirect(reverse('task_list'))
+
+	return {
+		'form': form,
+		'TEMPLATE': template,
 	}
