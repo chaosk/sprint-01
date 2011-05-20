f51e7765ff307a26a70553ea958aef93b79518d8
Krzysztof Socha <ksocha@ksocha.com>
2011-05-20 22:59:50 +0200

    added project skeleton

---
+++ sprint-01/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls.defaults import patterns, url, include, handler404, handler500
+from django.contrib import admin
+import settings
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+)
