2bd06c91bb7d2188cc295cb6e1ceed0d41d43ca3
Krzysztof Socha <ksocha@ksocha.com>
2011-05-21 02:16:37 +0200

    changed due_to type to date

--- sprint-01/tasks/models.py
+++ sprint-01/tasks/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Task(models.Model):
 	title = models.CharField(max_length=100)
-	created_at = models.DateTimeField(auto_now_add=True)
+	created_at = models.DateField(auto_now_add=True)
 	created_by = models.ForeignKey(User, related_name="tasks")
 	due_to = models.DateTimeField(blank=True, null=True)
 	description = models.TextField(blank=True)
